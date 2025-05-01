55b75522aab5b68876a5c4381ef19ad465107a33
Damian Fandiño <dfandino@Mac-mini-de-Damian.local>
2025-05-01 10:13:24 -0500

    fix name parameter repository 'Producto'

--- src/main/java/com/seti/franquiciasapi/repository/ProductoRepository.java
+++ src/main/java/com/seti/franquiciasapi/repository/ProductoRepository.java
@@ -1,5 +1,6 @@
 package com.seti.franquiciasapi.repository;
 
+import com.seti.franquiciasapi.entities.Producto;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ProductoRepository extends CrudRepository<Producto, Long> {
