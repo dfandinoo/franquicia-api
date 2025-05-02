b68c798a153ae31f5fb1b7f01ee6b28b29ce9504
Damian Fandiño <dfandino@Mac-mini-de-Damian.local>
2025-05-01 20:27:14 -0500

    add request mapping

--- src/main/java/com/seti/franquiciasapi/controller/FranquiciaController.java
+++ src/main/java/com/seti/franquiciasapi/controller/FranquiciaController.java
@@ -6,15 +6,13 @@ import com.seti.franquiciasapi.servicies.FranquiciaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/franquicias/")
 public class FranquiciaController {
 
     @Autowired
